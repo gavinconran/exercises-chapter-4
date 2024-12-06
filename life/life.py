48f6430c7d23856f54fdea87b9ff0cf987560eee
gavinconran <gtconran@gmail.com>
2024-12-06 21:00:18 +0000

    flake8 recommendations

--- life/life.py
+++ life/life.py
@@ -2,14 +2,8 @@ import numpy as np
 from matplotlib import pyplot
 from scipy.signal import convolve2d
 
-glider = np.array([[0,1,0],[0,0,1],[1,1,1]])
-
-blinker = np.array([
-[0,0,0],
-[1,1,1],
-[0,0,0]]
-    )
-
+glider = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
+blinker = np.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
 glider_gun = np.array([
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
     [0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -49,30 +43,41 @@ glider_gun = np.array([
     [0, 0, 1, 1, 0, 0, 0, 0, 0]
 ])
 
+
 class Game:
-    def __init__(game,Size):
-        game.board = np.zeros((Size, Size))
+    """The game class reprents a circle."""
+
+    def __init__(self, size):
+        """Game class constructor method."""        
+        self.board = np.zeros((size, size))
     
     def play(self):
+        """Starts the game."""        
         print("Playing life. Press ctrl + c to stop.")
         pyplot.ion()
         while True:
-            self.move(); self.show()
+            self.move()
+            self.show()
             pyplot.pause(0.0000005)
 
     def move(self):
+        """Moves the game."""        
         STENCIL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
         NeighbourCount = convolve2d(self.board, STENCIL, mode='same')
-
         for i in range(self.board.shape[0]):
             for j in range(self.board.shape[1]):
-                self.board[i,j]=1 if (NeighbourCount[i, j]==3 or (NeighbourCount[i, j]==2 and self.board[i,j])) else 0
+                self.board[i, j] = 1 \
+                    if (NeighbourCount[i, j] == 3 or
+                        (NeighbourCount[i, j] == 2 and 
+                         self.board[i, j])) else 0
 
     def __setitem__(self, key, value): 
+        """Sets an item."""        
         self.board[key] = value
 
-
     def show(self):
+        """Shows the game."""        
         pyplot.clf() 
         pyplot.matshow(self.board, fignum=0, cmap='binary')
         pyplot.show()
+
