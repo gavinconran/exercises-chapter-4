90a3cad117f53bbb65bc8c262808f29e1a3413af
gavinconran <gtconran@gmail.com>
2024-12-07 14:02:04 +0000

    __init__ # noqa F401

--- life/__init__.py
+++ life/__init__.py
@@ -1,3 +1,3 @@
 """Contains the life __init__ file Ex 4.3 of OOP4Maths."""
-from life.life import Pattern
-# from .life import glider, blinker, glider_gun, Game
+from life.life import Pattern # noqa F401
+from .life import glider, blinker, glider_gun, Game # noqa F401
